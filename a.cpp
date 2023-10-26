4545c8883df905ab0607465a79423083a0b049b7
QinghaoHu <102684048+QinghaoHu@users.noreply.github.com>
2023-10-26 19:07:29 -0400

    Add files via upload

---
+++ a.cpp
@@ -0,0 +1,31 @@
+﻿#include <algorithm>
+#include <cstring>
+#include <climits>
+#include <iostream>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <map>
+#include <set>
+#include <cmath>
+#include <fstream>
+#include <iomanip>
+#include <queue>
+#include <list>
+#include <stack>
+#include <array>
+#include <cstdio>
+
+typedef long long ll;
+
+using namespace std;
+
+int main(){
+	//setOI();
+	ios::sync_with_stdio(0);
+	cin.tie (0);
+
+	
+
+	return 0;
+}
